5f1a893767e9a31463dbdb9726fbecb6e79cc3c4
Dhanushka <dhanushka@jgtsltd.co.uk>
2026-06-06 14:38:20 +0530

    Intial Commit

---
+++ iOS_Demo/iOS_Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_Demo
+//
+//  Created by Dhanushka Jayakody on 2026-06-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_Demo/iOS_Demo/iOS_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_DemoApp.swift
+//  iOS_Demo
+//
+//  Created by Dhanushka Jayakody on 2026-06-06.
+//
+
+import SwiftUI
+
+@main
+struct iOS_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
